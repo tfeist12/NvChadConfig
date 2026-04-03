78818dec9ccf355f289d93ce9dde92814f4d00d0
Tyler Feist <tfeist612@gmail.com>
2026-04-03 16:23:11 -0600

    tmux copy debounce

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,47 @@
 require "nvchad.options"
 
--- add yours here!
+-- Show relative line numbers
+vim.o.relativenumber = true
 
-local o = vim.o
-o.relativenumber = true -- show relative line numbers
+-- Fix single character deletion hanging issue
+local copy_timer = nil
+local copy_text = nil
+
+local function debounced_copy(lines)
+  copy_text = table.concat(lines, "\n")
+  if copy_timer then
+    copy_timer:stop()
+    copy_timer:close()
+    copy_timer = nil
+  end
+  local timer = vim.uv.new_timer()
+  if not timer then
+    return
+  end
+  copy_timer = timer
+  timer:start(
+    150,
+    0,
+    vim.schedule_wrap(function()
+      vim.fn.system({ "tmux", "load-buffer", "-w", "-" }, copy_text)
+      if timer then
+        timer:close()
+      end
+      if copy_timer == timer then
+        copy_timer = nil
+      end
+    end)
+  )
+end
+
+vim.g.clipboard = {
+  name = "tmux-debounced",
+  copy = {
+    ["+"] = debounced_copy,
+    ["*"] = debounced_copy,
+  },
+  paste = {
+    ["+"] = { "tmux", "save-buffer", "-" },
+    ["*"] = { "tmux", "save-buffer", "-" },
+  },
+}
